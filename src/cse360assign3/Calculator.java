b22ececde06792dd589b5adf07ecf0d120c9b28c
programmingprincess <jiaqiwu@asu.edu>
2016-02-21 15:07:21 -0700

    initial-commit
    Create the initial version
    a. Create the package cse360assign3.
    b. Copy the file Calculator.java to this package. Document this file
    using the
    Javadoc comment style but do not make any code changes to the file at
    this time.
    c. Generate the Junit tests for this code. There should be seven test
    stub methods
    generated, one for each method. All but the testCalculator method
    should include the default fail assert. The testCalculator method
    should test to make sure that the constructor does not return a null.
    d. Commit the project to git and github

---
+++ src/cse360assign3/Calculator.java
@@ -0,0 +1,69 @@
+/** Calculator allows user to add, subtract, divide, multiply, 
+ *  see the total and get the calculation history. 
+ *  
+ * @author Jiaqi Wu
+ * PIN: 908
+ * @version February 21, 2016
+ */
+
+package cse360assign3;
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Constructor for Calculator
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Method that returns user's total value
+	 * @return total value
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds value to total
+	 * @param value to be added
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtracts value from total
+	 * @param value to be subtracted 
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Multiplies the current total by value
+	 * @param multiplying factor
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * Divides current total by value
+	 * @param value to divide by 
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * Get calculation history
+	 * @return string with past calculations
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ src/cse360assign3/CalculatorTester.java
@@ -0,0 +1,48 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTester {
+
+	@Test
+	public void testCalculator() {
+		Calculator ti84 = new Calculator();
+		
+		assertNotEquals(ti84, null);
+	}
+
+	
+	@Test
+	public void getTotal() {
+		fail("Not yet implemented");
+	}
+	
+	@Test 
+	public void add() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void subtract() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void multiply () {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void divide () {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void getHistory () {
+		fail("Not yet implemented");
+	}
+	
+	
+}
